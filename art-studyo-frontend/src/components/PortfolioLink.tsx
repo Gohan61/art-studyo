5d19f7f41ff08ed230baf60c611561a178198535
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-02-17 16:34:48 +0100

    Refactor: uniform button design + link to shop

--- art-studyo-frontend/src/components/PortfolioLink.tsx
+++ art-studyo-frontend/src/components/PortfolioLink.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function PortfolioLink() {
   return (
-    <div className="col-start-1 row-start-1 border-2 rounded-md mt-4 shadow-2xl shadow-cyan-600 bg-gradient-to-r from-emerald-700 to-green-700 hover:bg-gradient-to-r hover:from-emerald-600 hover:to-green-600 size-fit p-4 self-end justify-self-center mb-3">
+    <div className="col-start-1 row-start-2 border-2 rounded-3xl mt-4 shadow-xl shadow-blue-950 bg-gradient-to-r from-blue-950 to-blue-900 hover:bg-gradient-to-r hover:from-blue-900 hover:to-blue-950 size-fit p-4 self-center justify-self-center mb-3">
       <Link
         to={"/portfolio"}
-        className="text-xl md:text-2xl text-white font-bold italic"
+        className="text-2xl md:text-4xl text-white font-bold italic"
       >
-        My portfolio
+        Portfolio
       </Link>
     </div>
   );
--- art-studyo-frontend/src/components/ShopLink.tsx
+++ art-studyo-frontend/src/components/ShopLink.tsx
@@ -1,17 +1,17 @@
-import etsy from "../assets/Etsy_logo.svg";
+import etsyShopImage from "../assets/Screenshot from 2025-02-17 16-00-44.png";
 
 export default function ShopLink() {
   return (
-    <div className="col-start-2 row-start-3 flex flex-col items-center">
+    <div className="col-start-2 row-start-3 flex flex-col place-self-center border-2 rounded-3xl shadow-xl shadow-blue-950 bg-gradient-to-r from-blue-950 to-blue-900 hover:bg-gradient-to-r hover:from-blue-900 hover:to-blue-950 w-fit h-fit p-3 mb-2 sm:mb-4">
       <a
-        href=""
+        href="https://www.etsy.com/shop/Selenartstudyo"
         rel="noreferrer noopener"
-        className="border-2 rounded-md mt-4 bg-blend-darken shadow-2xl shadow-cyan-700 bg-cyan-950 hover:bg-cyan-900"
+        className=""
       >
-        <h2 className="text-md md:text-xl text-white font-bold mb-auto p-2">
-          Visit my shop on
+        <h2 className="text-lg md:text-2xl text-white font-bold">
+          My Etsy Shop
         </h2>
-        <img src={etsy} alt="Etsy" className="object-contain" />
+        <img src={etsyShopImage} alt="Etsy" className="object-contain w-2xs" />
       </a>
     </div>
   );

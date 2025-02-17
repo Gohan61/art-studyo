0b2c437eb414b34827c8eea69e0c77cf5cc7d015
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-02-17 16:29:40 +0100

    Refactor: change style buttons + alignment images
    * Instead of setting a background images, svg's are loaded fixed because
    it is easier to style and looks better
    * Reposition buttons to incorporate svg alignment
    * Position studyo logo in bottom corner
    * Apply border on homepage title to div instead of title, so animation
    is not applied to div

--- art-studyo-frontend/src/components/HomePage.tsx
+++ art-studyo-frontend/src/components/HomePage.tsx
@@ -1,13 +1,15 @@
 import PortfolioLink from "./PortfolioLink";
 import ShopLink from "./ShopLink";
+import paletteImage from "../assets/paint-palette-artist-svgrepo-com.svg";
+import brushImage from "../assets/paintbrush-svgrepo-com.svg";
 
 export default function HomePage() {
   return (
-    <div className="w-full min-h-screen bg-paint flex flex-col justify-center items-center">
-      <div className="w-full grid grid-cols-2 grid-rows-3">
+    <div className="w-full min-h-screen bg-paint flex flex-col justify-start items-center">
+      <div className="w-full grid grid-cols-2 grid-rows-3 z-10">
         <PortfolioLink></PortfolioLink>
-        <div className="flex flex-col items-center col-start-1 col-end-3 row-start-2 self-center">
-          <h1 className="animate-fade-in-scale text-4xl sm:text-6xl md:text-8xl text-white font-bold text-center bg-neutral-800 rounded-l-full border-2 border-neutral-800 block w-full">
+        <div className="flex flex-col items-center col-start-1 col-end-3 row-start-1 self-center bg-neutral-800 rounded-l-full border-2 border-neutral-800">
+          <h1 className="animate-fade-in-scale text-4xl sm:text-6xl md:text-8xl text-white font-bold text-center block w-full">
             Welcome to
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-800 to-purple-800 block">
               Selenart-Studyo
@@ -16,6 +18,21 @@ export default function HomePage() {
         </div>
         <ShopLink></ShopLink>
       </div>
+      <img
+        src={paletteImage}
+        alt=""
+        className="fixed z-0 w-3xs sm:w-md top-[45%] sm:top-[40%] md:top-[35%] left-0"
+      />
+      <img
+        src="https://res.cloudinary.com/du5var2so/image/upload/f_auto,q_auto/v1/selenart/be8a02daljyti2nbiour"
+        alt=""
+        className="contain w-2xs sm:w-lg rounded-sm mt-auto ml-auto z-10"
+      />
+      <img
+        src={brushImage}
+        alt=""
+        className="fixed z-0 w-sm sm:w-md md:w-lg top-0 right-0"
+      />
     </div>
   );
 }
